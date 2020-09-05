7b53404893886c1b16278a31962bbdb70eb7f662
meta-jai <52705143+meta-jai@users.noreply.github.com>
2020-09-05 23:10:08 +0530

    Initial

---
+++ Web_AbstractFactoryMethod/Scooty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_AbstractFactoryMethod
+{
+    /// <summary>
+    /// The 'ProductB2' class
+    /// </summary>
+    class Scooty : IScooter
+    {
+        public string Name()
+        {
+            return "Scooty- Name";
+        }
+    }
+}
---
+++ Web_AbstractFactoryMethod/SportsBike.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web_AbstractFactoryMethod
+{
+    /// <summary>
+    /// The 'ProductA2' class
+    /// </summary>
+    class SportsBike : IBike
+    {
+        public string Name()
+        {
+            return "Sports Bike- Name";
+        }
+    }
+}
